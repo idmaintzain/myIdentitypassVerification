313a4dad126c215624350694052a6c1198e90f67
idmaintzain <iadeyinka03@gmail.com>
2022-01-19 15:48:16 +0100

    update

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 include('MyIdentityPass.php');
 $ver = new MyIdentityPass;
-define("URL", "https://api.myidentitypay.com/api/v1/biometrics/merchant/data/verification/nin_wo_face");
-define("API_CODE", "exxxxxxxx.xxxxxxxxxxxxxxxxx");
+define("URL", "https://api.myidentitypay.com/api/v1/biometrics/merchant/data/verification/nin_wo_face"); // myidentitypass api endpoint
+define("API_CODE", "exxxxxxxx.xxxxxxxxxxxxxxxxx"); // X-API-Key secret key
 $number = 'xxxxxxxxx'; //variable $number is verification number eg, NIN, BVN
-$response = $ver->dataVerification($number,URL,API_CODE);
+$response = $ver->dataVerification( $number, URL, API_CODE);
 $ver->printArray($response);
 
 
